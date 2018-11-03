63caddd602963f7e527199353799e1e98ef8b38c
Rudy Sharar <rudysharar@D-10-19-164-166.dhcp4.washington.edu>
2018-11-03 09:50:33 -0700

    server listening on localhost and loading home.html route

---
+++ server.js
@@ -0,0 +1,24 @@
+// ---- DEPENDENCIES ----- //
+var express = require('express');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// get route to load home.html
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+ })
+
+
+
+ //test connection to server
+ app.listen(PORT,function(){
+    console.log("Server is listening on PORT: " + PORT);
+ })
