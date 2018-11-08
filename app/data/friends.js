197e4475f0f2a819fd4698f2c00124a9ef5f9b0e
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-07 21:25:22 -0800

    added sample data in friends array and modeule export data to apiroutes

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+// friends data array
+
+var friends = [
+    {
+        name: "Rudy",
+        photo: "https://yt3.ggpht.com/a-/ACSszfGctGmgKQbGg4vvImfo-5xaOWTAA4Tc21T1nw=s900-mo-c-c0xffffffff-rj-k-no",
+        scores: [3,5,1,2,4,5,2,4,1,5]
+    }
+]
+
+module.exports = friends;
+
