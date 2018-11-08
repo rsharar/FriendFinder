bd8b38c40898ee73e2364d518f692c0e39dabbaf
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-07 21:47:11 -0800

    all endpoints loading on localhost

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,39 +1,26 @@
-// ---- DEPENDENCIES ----- //
-var express = require('express');
-var path = require('path');
+// require data from friends.js
+var friends = require("../data/friends")
 
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3030;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// empty friends array
-var friends = []
-
-// get route to load home.html
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-})
-
-// GET route to API of all friends
+module.exports = function(app){
+// GET request to API of all friends
 app.get("/api/friends", function (req, res) {
     return res.json(friends);
 });
-
-// add newFriend object data to friends array
+// POST request to add newFriend object data to friends array
 app.post("/api/friends", function (req, res) {
+    // store data from user survey in variable newFriend
     var newFriend = req.body;
     friends.push(newFriend);
     res.json(newFriend);
 });
 
+}
 
-//test connection to server
-app.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT);
-})
+
+
+
+
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,6 @@
-// ---- DEPENDENCIES ----- //
-var express = require('express');
+// Required path dependency
 var path = require('path');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3030;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 module.exports = function (app) {
 
@@ -21,10 +12,4 @@ module.exports = function (app) {
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     })
-
-
 }
-//test connection to server
-app.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT);
-})
