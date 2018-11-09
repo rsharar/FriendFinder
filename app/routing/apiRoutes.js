5961749c23c94d199e89784b85cc45d499c04621
Rudy Sharar <rudysharar@D-10-18-193-149.dhcp4.washington.edu>
2018-11-08 18:33:34 -0800

    psuedo code for calculating difference between newFriend and friends to determine best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,6 @@
 // require data from friends.js
 var friends = require("../data/friends")
 
-
-
-
 module.exports = function(app){
 // GET request to API of all friends
 app.get("/api/friends", function (req, res) {
@@ -15,16 +12,24 @@ app.post("/api/friends", function (req, res) {
     var newFriend = req.body;
     // push to friends array
     friends.push(newFriend);
-    var newFriendScores = newFriend.scores.map(function(n){
-        return parseInt(n);
-    })
-    //store comparison in array
-    var comparisonArray = [];
+    // CALC SUM OF NEW FRIEND SCORES
+    // convert survey responses to integers
+    var newFriendTotalScore = 0;
+    for (var j=0; j<newFriend.scores; j++){
+        parseInt(newFriend.scores[j])
+        newFriendTotalScore = newFriendTotalScore + newFriend.scores[j];
+    }
+
+    // CALC SUM OF FRIENDS SCORES
     for (i in friends.length){
         // declare variable to store scores of friends currently in DB
         var dbFriend = friends[i].scores;
+        console.log(dbFriend);
         
     }
+
+    // FIND SMALLEST DIFFERENCE IN TOTAL SCORES
+    // RETURN THE 'BEST MATCH' in modal
 });
 
 }
