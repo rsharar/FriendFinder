2e025b8e94d89fb063e969fd51ed61538d9184f7
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-08 22:21:17 -0800

    added obama

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,7 +4,22 @@ var friends = [
     {
         name: "Rudy",
         photo: "https://yt3.ggpht.com/a-/ACSszfGctGmgKQbGg4vvImfo-5xaOWTAA4Tc21T1nw=s900-mo-c-c0xffffffff-rj-k-no",
-        scores: [3,5,1,2,4,5,2,4,1,5]
+        scores: [1,1,1,2,2,2,2,1,1,1]
+    },
+    {
+        name: "Gary Payton",
+        photo: "https://www.gannett-cdn.com/-mm-/8177e1eeb06a934d4135ff6c395a147b5d96b563/c=0-261-1148-1124/local/-/media/USATODAY/USATODAY/2013/04/05/xxx-nba-playoffs_-lakers_sonics.jpg?width=534&height=401&fit=crop",
+        scores: [3,3,3,3,2,4,3,2,1,2]
+    },
+    {
+        name: "Lisa Simpson",
+        photo: "https://vignette.wikia.nocookie.net/simpsons/images/5/57/Lisa_Simpson2.png/revision/latest?cb=20180319000458",
+        scores: [5,4,4,3,5,5,4,5,5,5]
+    },
+    {
+        name: "Barack Obama",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
+        scores: [4,2,3,1,3,2,4,1,1,5]
     }
 ]
 
