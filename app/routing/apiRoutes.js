9b6c8a86cd7b3218ccfdf7499bfb21435ec2cb3a
Rudy Sharar <rudysharar@Rudys-MacBook-Pro-2.local>
2018-11-08 16:14:38 -0800

    added package.json and gitignore

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,9 @@ app.get("/api/friends", function (req, res) {
 app.post("/api/friends", function (req, res) {
     // store data from user survey in variable newFriend
     var newFriend = req.body;
+    // push to friends array
     friends.push(newFriend);
+    // return JSON of friends array
     res.json(newFriend);
 });
 
